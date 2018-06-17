6d88c84e0b9404939c4bdd32a6a3e68f8efedf99
Lisa M. Venezia <leftylmv78@gmail.com>
2018-06-17 15:11:35 -0400

    feat: Put open card on the openCard list

--- js/app.js
+++ js/app.js
@@ -20,6 +20,8 @@ const cardDeck = [
   "fa-bomb"
 ];
 
+let openList = [];
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -64,6 +66,20 @@ function showCard(card) {
    return;
 }
 
+// Add to list of open cards
+function addToOpenList(index){
+  let currentCard = {location:index, symbol:shuffledDeck[index]};
+  let found = openList.find(function(element) {
+    return element.location === currentCard.location;
+  });
+
+  if(typeof found === 'undefined'){
+    openList.push(currentCard);
+    console.log(currentCard);
+  }
+  return;
+}
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol
@@ -72,11 +88,18 @@ for(let i = 0; i < cardHTML.length; i++) {
   cardHTML[i].addEventListener("click", function(element) {
     console.log(element)
     showCard(element);
+
+    /*
+     *  - add the card to a *list* of "open" cards
+     */
+
+     addToOpenList(i);
+
   });
 }
 
- /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
+
+ /*  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
