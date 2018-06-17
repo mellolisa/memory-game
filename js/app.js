646be43f0b5bceec19ef0d74bd5ed3af24f188d0
Lisa M. Venezia <leftylmv78@gmail.com>
2018-06-17 12:58:09 -0400

    feat: Add ability to show cards.

--- js/app.js
+++ js/app.js
@@ -56,11 +56,26 @@ function shuffle(array) {
     return array;
 }
 
+// Display a card's symbol
+function showCard(card) {
+   if(card.target.className.includes("open show") === false){
+     card.target.className += ' open show';
+   }
+   return;
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - display the card's symbol
+ */
+for(let i = 0; i < cardHTML.length; i++) {
+  cardHTML[i].addEventListener("click", function(element) {
+    console.log(element)
+    showCard(element);
+  });
+}
+
+ /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
